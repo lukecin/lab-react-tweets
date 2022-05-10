1f4e4bd74d6817090d3a38453bf670778ab6320f
Lukecin <dluqueru@gmail.com>
2022-05-10 09:34:50 +0200

    done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,60 +1,37 @@
+import ProfileImage from "./ProfileImage"
+import User from "./User"
+import Timestamp from "./Timestamp"
+import Message from "./Message"
+import Actions from "./Actions"
+
 function Tweet(props) {
-  return (
-    <div className="tweet">
-      {/* <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+    return (
+      <div>
+      <div className="tweet">
+        <img
+          src="https://i.imgur.com/9yw1Fyw.jpg"
+          className="profile"
+          alt="profile"
+        />
+  
+          <ProfileImage image={props.tweet.user.image} />
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          <User userData={props.tweet.user}/>
+  
+            <Timestamp time={props.tweet.timestamp}/>
+          </div>
+              
+             <Message message={props.tweet.message}/>
+  
+          <div className="actions">
+            <i class="far fa-comment"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="far fa-heart"></i>
+            <i class="fas fa-share"></i>
+          </div>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <Actions />
+
         </div>
-
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-    
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
-      </div>
-
-      <i class="fas fa-ellipsis-h"></i> */}
-      <img src={props.tweet.user.image} className="profile" />
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">{props.tweet.user.name}</span>
-            <span className="handle">{props.tweet.user.handle}</span>
-          </span>
-          
-          <span className="timestamp">{props.tweet.timestamp}</span>
-        </div>
-      </div>
-      <p className="message">
-         {props.tweet.message}
-      </p>
-      <div className="actions">
-    
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-      </div>
-    </div>
-  );
-}
-
-export default Tweet;
+    );
+  }
